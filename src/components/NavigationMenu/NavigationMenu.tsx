c81f31aa4d220dd496eb5f260beaf4ec175daf65
Galina Bobrik <galagrinko@gmail.com>
2025-02-26 15:01:42 +0400

    style: move colors to separate colors.scss

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,25 +1,79 @@
+// 'use client';
+
+// import NavLink from './NavLink/NavLink';
+
+// export const NavigationMenu = () => {
+//     return (
+//         <nav className="flex justify-center pt-6 pb-6 bg-amber-300 rounded-3xl mx-12 my-4 backdrop-blur-[20px] bg-gradient-to-br from-white/80 to-white/20">
+//             <ul className="flex flex-wrap items-center gap-3 text-sm font-medium md:gap-8">
+//                 <li>
+//                     <NavLink
+//                         href="/"
+//                         exact
+//                         className="inline-flex rounded-full px-5 py-1.5 text-slate-500 hover:text-indigo-500 [&.active]:bg-slate-200 [&.active]:text-indigo-600"
+//                     >
+//                         Домой
+//                     </NavLink>
+//                 </li>
+//                 <li>
+//                     <NavLink
+//                         href="/arrays"
+//                         className="inline-flex rounded-full px-5 py-1.5 text-slate-500 hover:text-indigo-500 [&.active]:bg-slate-200 [&.active]:text-indigo-600"
+//                     >
+//                         Массивы
+//                     </NavLink>
+//                 </li>
+//             </ul>
+//         </nav>
+//     );
+// };
+
 'use client';
 
 import NavLink from './NavLink/NavLink';
+import styles from './NavigationMenu.module.scss';
+
+// 'use client';
+
+// import NavLink from './NavLink/NavLink';
+
+// export const NavigationMenu = () => {
+//     return (
+//         <nav className="flex justify-center pt-6 pb-6 bg-amber-300 rounded-3xl mx-12 my-4 backdrop-blur-[20px] bg-gradient-to-br from-white/80 to-white/20">
+//             <ul className="flex flex-wrap items-center gap-3 text-sm font-medium md:gap-8">
+//                 <li>
+//                     <NavLink
+//                         href="/"
+//                         exact
+//                         className="inline-flex rounded-full px-5 py-1.5 text-slate-500 hover:text-indigo-500 [&.active]:bg-slate-200 [&.active]:text-indigo-600"
+//                     >
+//                         Домой
+//                     </NavLink>
+//                 </li>
+//                 <li>
+//                     <NavLink
+//                         href="/arrays"
+//                         className="inline-flex rounded-full px-5 py-1.5 text-slate-500 hover:text-indigo-500 [&.active]:bg-slate-200 [&.active]:text-indigo-600"
+//                     >
+//                         Массивы
+//                     </NavLink>
+//                 </li>
+//             </ul>
+//         </nav>
+//     );
+// };
 
 export const NavigationMenu = () => {
     return (
-        <nav className="flex justify-center pt-6 pb-6 bg-amber-300 rounded-3xl mx-12 my-4 backdrop-blur-[20px] bg-gradient-to-br from-white/80 to-white/20">
-            <ul className="flex flex-wrap items-center gap-3 text-sm font-medium md:gap-8">
+        <nav className={styles.navigation}>
+            <ul className={styles.navigationList}>
                 <li>
-                    <NavLink
-                        href="/"
-                        exact
-                        className="inline-flex rounded-full px-5 py-1.5 text-slate-500 hover:text-indigo-500 [&.active]:bg-slate-200 [&.active]:text-indigo-600"
-                    >
+                    <NavLink href="/" exact className={styles.navigationLink}>
                         Домой
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink
-                        href="/arrays"
-                        className="inline-flex rounded-full px-5 py-1.5 text-slate-500 hover:text-indigo-500 [&.active]:bg-slate-200 [&.active]:text-indigo-600"
-                    >
+                    <NavLink href="/arrays" className={styles.navigationLink}>
                         Массивы
                     </NavLink>
                 </li>
