fe39b928da1063f2ec7df54da34da694bb6370a3
Galina Bobrik <galagrinko@gmail.com>
2025-03-06 14:24:46 +0400

    feat: add HomePageImage

---
+++ src/entities/homePageImage/ui/HomePageImage.tsx
@@ -0,0 +1,5 @@
+import Image from 'next/image';
+
+export const HomePageImage = () => {
+    return <Image src="/images/mainPandaImg.png" alt="Картинка панды" width={300} height={300} />;
+};
--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,8 +1,11 @@
 import { HexGallery } from 'widgets/HexGallery/index';
 
+import { HomePageImage } from '@/entities/homePageImage/ui/HomePageImage';
+
 export default function HomePage() {
     return (
         <>
+            <HomePageImage />
             <HexGallery />
         </>
     );
