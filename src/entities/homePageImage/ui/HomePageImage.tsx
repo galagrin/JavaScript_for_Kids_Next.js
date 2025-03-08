b83dc693684ba8e23aa66e1560586f863c758ced
Galina Bobrik <galagrinko@gmail.com>
2025-03-08 17:27:11 +0400

    ref: moved Image to page component

--- src/entities/homePageImage/ui/HomePageImage.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export const HomePageImage = () => {
-    return <Image src="/images/mainPandaImg.png" alt="Картинка панды" width={300} height={300} />;
-};
--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,11 +1,10 @@
+import Image from 'next/image';
 import { HexGallery } from 'widgets/HexGallery/index';
-
-import { HomePageImage } from '@/entities/homePageImage/ui/HomePageImage';
 
 export default function HomePage() {
     return (
         <>
-            <HomePageImage />
+            <Image src="/images/mainPandaImg.png" alt="Картинка панды" width={300} height={300} />;
             <HexGallery />
         </>
     );
