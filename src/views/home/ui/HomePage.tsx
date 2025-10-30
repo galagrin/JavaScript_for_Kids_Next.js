82289dbf6af13d789df0f829060ad59229d668c9
Galina Bobrik <galagrinko@gmail.com>
2025-10-30 18:04:04 +0400

    ref: edit image to improve LCP

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -1,44 +1,26 @@
-'use client';
-
 import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
 
 import { HexGallery } from '@/widgets/HexGallery';
 
 import styles from './HomePage.module.scss';
 
 export default function HomePage() {
-    const [showBubble, setShowBubble] = useState(false);
-    const timerRef = useRef<number | null>(null);
-
-    const handleImageLoad = () => {
-        // Задержка перед показом speechBubble, например 1.5 секунды
-        setTimeout(() => {
-            setShowBubble(true);
-        }, 1500);
-    };
-    useEffect(
-        () => () => {
-            if (timerRef.current) clearTimeout(timerRef.current);
-        },
-        []
-    );
     return (
         <div className={styles.container}>
             <div className={styles.pandaBlock}>
-                <div className={`${styles.speechBubble} ${showBubble ? styles.visible : ''}`}>
+                <div className={`${styles.speechBubble} ${styles.visible}`}>
                     Привет! Я Панда. Здесь ты можешь выбрать раздел перейти на страницу, чтобы учить JavaScript.
                     <br />
                     Просто нажми на любую цветную плитку!
                 </div>
                 <Image
-                    src="/images/mainPandaImg.png"
+                    src="/images/mainPandaImg.webp"
                     alt="Картинка панды"
                     width={300}
                     height={300}
                     className={styles.pandaImage}
-                    onLoad={handleImageLoad}
                     priority
+                    sizes="(max-width: 768px) 200px, 300px"
                 />
             </div>
             <HexGallery />
